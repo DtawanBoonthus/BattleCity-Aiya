4423ac5739515299af9196815009d3766469a243
buntad <dtawan.arunsawad@gmail.com>
2025-12-07 17:37:15 +0700

    add `INetworkContext` interface and `NetworkContext` implementation, integrate with global lifetime scope

--- Assets/Project/Scripts/Bootstrap/GlobalLifetimeScope.cs
+++ Assets/Project/Scripts/Bootstrap/GlobalLifetimeScope.cs
@@ -1,10 +1,19 @@
-﻿namespace BC.Bootstrap
+﻿using BC.Shared.Networks;
+using Mirage;
+using UnityEngine;
+
+namespace BC.Bootstrap
 {
     public class GlobalLifetimeScope : LifetimeScope
     {
+        [SerializeField] private NetworkManager networkManager = null!;
+
         protected override void Configure(IContainerBuilder builder)
         {
             new InputInstaller().Install(builder);
+
+            var networkContext = new NetworkContext(networkManager);
+            builder.RegisterInstance<INetworkContext>(networkContext);
         }
     }
 }
---
+++ Assets/Project/Scripts/Shared/Networks/Interfaces/INetworkContext.cs
@@ -0,0 +1,16 @@
+﻿using Mirage;
+
+namespace BC.Shared.Networks;
+
+public interface INetworkContext
+{
+    NetworkManager NetworkManager { get; }
+    NetworkServer Server { get; }
+    NetworkClient Client { get; }
+    NetworkSceneManager NetworkSceneManager { get; }
+    ServerObjectManager ServerObjectManager { get; }
+    ClientObjectManager ClientObjectManager { get; }
+    bool IsServer { get; }
+    bool IsClient { get; }
+    bool IsHost { get; }
+}
