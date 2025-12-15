a5270ca2acf80d44b9caa4abccdc20797d3bd3da
buntad <dtawan.arunsawad@gmail.com>
2025-12-15 15:54:03 +0700

    add `GameControllerRouter` and refactor `GameController` to support asynchronous game start handling with improved routing, lifecycle management, and countdown logic

--- Assets/Project/Scripts/Gameplay/GameSessions/GameController.cs
+++ Assets/Project/Scripts/Gameplay/GameSessions/GameController.cs
@@ -1,43 +1,64 @@
-﻿using BC.Gameplay.Configs;
-using BC.Shared.Networks;
-using BC.Shared.Spawners;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BC.Gameplay.Configs;
 using Cysharp.Threading.Tasks;
+using Mirage;
+using R3;
 using UnityEngine;
 using VContainer;
-using VitalRouter;
 
-namespace BC.Gameplay;
-
-[Routes]
-public partial class GameController
+namespace BC.Gameplay
 {
-    [Inject] private readonly INetworkContext networkContext = null!;
-    [Inject] private readonly IGameplayConfig gameplayConfig = null!;
-    [Inject] private readonly ISpawnService<MirageNet> spawnService = null!;
-
-    private IGameStartCountdown countdown = null!;
-
-    private const string LOG_PREFIX = $"[{nameof(GameController)}]";
-
-    [Route]
-    private UniTask OnGameStartServerCommand(GameStartServerCommand gameStartServerCommand)
+    public class GameController : NetworkBehaviour
     {
-        var gameStartCountdown = spawnService.Spawn(gameplayConfig.GameStartCountdownPrefab, Vector3.zero, Quaternion.identity);
-        countdown = gameStartCountdown.GetComponent<IGameStartCountdown>();
-        return UniTask.CompletedTask;
-    }
+        [Inject] private readonly GameControllerRouter router = null!;
+        [Inject] private readonly IGameplayConfig gameplayConfig = null!;
 
-    [Route]
-    private async UniTask OnGameStartCommand(GameStartCommand gameStartCommand)
-    {
-        if (!networkContext.IsServer)
+        private CompositeDisposable? disposables;
+        private CompositeDisposable? destroyDisposable;
+        private const string LOG_PREFIX = $"[{nameof(GameController)}]";
+
+        private void Start()
         {
-            return;
+            destroyDisposable ??= new CompositeDisposable();
+            router.OnGameStart.SubscribeAwait(OnGameStartCommand).AddTo(destroyDisposable);
         }
-        
-        //Hard coded delay
-        await UniTask.Delay(1000);
-        await countdown.StartCountdownAsync(gameplayConfig.StartGameDurationSecond);
-        Debug.Log($"{LOG_PREFIX} Game Started!");
+
+        private void OnDestroy()
+        {
+            destroyDisposable?.Dispose();
+            destroyDisposable = null;
+        }
+
+        private void OnEnable()
+        {
+            disposables ??= new CompositeDisposable();
+        }
+
+        private void OnDisable()
+        {
+            disposables?.Dispose();
+            disposables = null;
+        }
+
+        [Server]
+        private async ValueTask OnGameStartCommand(Unit _, CancellationToken cancellationToken)
+        {
+            if (!IsServer)
+            {
+                return;
+            }
+
+            if (router.GameStartCountdown == null)
+            {
+                Debug.LogError($"{LOG_PREFIX} GameStartCountdown is null.");
+                return;
+            }
+
+            //Hard coded delay
+            await UniTask.Delay(1000, cancellationToken: cancellationToken);
+            await router.GameStartCountdown.StartCountdownAsync(gameplayConfig.StartGameDurationSecond);
+            Debug.Log($"{LOG_PREFIX} Game Started!");
+        }
     }
 }
---
+++ Assets/Project/Scripts/Gameplay/GameSessions/GameControllerRouter.cs
@@ -0,0 +1,47 @@
+﻿using BC.Gameplay.Configs;
+using BC.Shared.Networks;
+using BC.Shared.Spawners;
+using Cysharp.Threading.Tasks;
+using R3;
+using UnityEngine;
+using VContainer;
+using VitalRouter;
+
+namespace BC.Gameplay;
+
+[Routes]
+public partial class GameControllerRouter
+{
+    [Inject] private readonly INetworkContext networkContext = null!;
+    [Inject] private readonly IGameplayConfig gameplayConfig = null!;
+    [Inject] private readonly ISpawnService<MirageNet> spawnService = null!;
+
+    private readonly Subject<Unit> gameStart = new();
+
+    public IGameStartCountdown? GameStartCountdown { get; private set; } = null;
+    public Observable<Unit> OnGameStart => gameStart;
+
+    private const string LOG_PREFIX = $"[{nameof(GameControllerRouter)}]";
+
+    [Route]
+    private UniTask OnGameStartServerCommand(GameStartServerCommand gameStartServerCommand)
+    {
+        var gameGameController = spawnService.Spawn(gameplayConfig.GameControllerPrefab, Vector3.zero, Quaternion.identity);
+
+        var gameStartCountdown = spawnService.Spawn(gameplayConfig.GameStartCountdownPrefab, Vector3.zero, Quaternion.identity);
+        GameStartCountdown = gameStartCountdown.GetComponent<IGameStartCountdown>();
+        return UniTask.CompletedTask;
+    }
+
+    [Route]
+    private UniTask OnGameStartCommand(GameStartCommand gameStartCommand)
+    {
+        if (!networkContext.IsServer)
+        {
+            return UniTask.CompletedTask;
+        }
+
+        gameStart.OnNext(Unit.Default);
+        return UniTask.CompletedTask;
+    }
+}
